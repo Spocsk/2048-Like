2aaa8f27eda02ce186b5bc445d6b0b070a9485fb
Dylan COUTO DE OLIVEIRA <dylandasilva79@gmail.com>
2025-11-10 18:05:05 +0100

    Initial Commit

---
+++ 2048/_048App.swift
@@ -0,0 +1,17 @@
+//
+//  _048App.swift
+//  2048
+//
+//  Created by Dylan on 10/11/2025.
+//
+
+import SwiftUI
+
+@main
+struct _048App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
